16866e052993268c8d4d6a8d410d9e2f434cfd38
brendonbainbridge <156622397+brendonbainbridge@users.noreply.github.com>
2025-01-14 18:59:20 -0700

    updates

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright-year').textContent += ` ${currentYear}`;
+
+// Get the last modified date
+const lastModified = new Date(document.lastModified);
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+document.getElementById('last-modified').textContent += ` ${lastModified.toLocaleDateString(undefined, options)}`;
