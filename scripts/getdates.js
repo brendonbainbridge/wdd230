c1d0859c65ba20041b47275c6b7856e747e41e83
brendonbainbridge <156622397+brendonbainbridge@users.noreply.github.com>
2025-01-23 22:12:36 -0700

    updates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,22 @@
 const currentYear = new Date().getFullYear();
 document.getElementById('copyright-year').textContent += ` ${currentYear}`;
 
-// Get the last modified date
-const lastModified = new Date(document.lastModified);
-const options = { year: 'numeric', month: 'long', day: 'numeric' };
-document.getElementById('lastModified').textContent += ` ${lastModified.toLocaleDateString(undefined, options)}`;
+// // Get the last modified date
+// const lastModified = new Date(document.lastModified);
+// const options = { year: 'numeric', month: 'long', day: 'numeric' };
+// document.getElementById('lastModified').textContent += ` ${lastModified.toLocaleDateString(undefined, options)}`;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+    const navList = document.querySelector(".navlist");
+
+    hamburgerMenu.addEventListener("click", () => {
+        navList.classList.toggle("show");
+        // Toggle the hamburger icon between ≡ and X
+        if (navList.classList.contains("show")) {
+            hamburgerMenu.textContent = "✕";
+        } else {
+            hamburgerMenu.textContent = "≡";
+        }
+    });
+});
