f0db9351a69b13350172111c7ec3370734ff1466
brendonbainbridge <156622397+brendonbainbridge@users.noreply.github.com>
2025-02-21 11:09:45 -0700

    update

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,9 +1,9 @@
 const visitsDisplay = document.querySelector(".visitnum");
 let numVisits = Number(window.localStorage.getItem("numOfvisits-ls")) || 0;
-if (numVisits !== 0) {
+if (numVisits !== 0 && ) {
     visitsDisplay.textContent = numVisits;
 } else {
-    visitsDisplay.textContent = `First time visiting my website! Thanks! :3`;
+    visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
 }
 numVisits++;
 localStorage.setItem("numOfvisits-ls", numVisits);
@@ -12,3 +12,7 @@ localStorage.setItem("numOfvisits-ls", numVisits);
 
 
 
+If this is the user's first visit, display "Welcome! Let us know if you have any questions.".
+If the amount of time between visits is less than a day, display "Back so soon! Awesome!".
+    Otherwise, display the number of days in a message like this: "You last visited n days ago.",
+     where n is the actual, whole number of days between visits.If the number of days is 1, then change the language to "day" not "days".
