d79ba1941bb44e10b7f569fee7db055d0dc2f3ae
brendonbainbridge <156622397+brendonbainbridge@users.noreply.github.com>
2025-02-22 11:22:19 -0700

    update

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,10 +1,7 @@
 const visitsDisplay = document.getElementById("visitnum");
 let numVisits = Number(window.localStorage.getItem("numOfvisits-ls")) || 0;
-
-// Calculate the number of days since the last visit
 const lastVisit = window.localStorage.getItem("lastvisit");
 const numdays = lastVisit ? Math.floor((Date.now() - new Date(lastVisit)) / (1000 * 60 * 60 * 24)) : 0;
-
 if (numVisits !== 0 && numdays > 0) {
     visitsDisplay.textContent = `You last visited ${numdays} days ago.`;
 } else if (numVisits !== 0) {
@@ -12,7 +9,6 @@ if (numVisits !== 0 && numdays > 0) {
 } else {
     visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
 }
-
 numVisits++;
 localStorage.setItem("numOfvisits-ls", numVisits);
 localStorage.setItem("lastvisit", Date.now());
