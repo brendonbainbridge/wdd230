5768609937e797021a107132ec6a2f0341aba936
brendonbainbridge <156622397+brendonbainbridge@users.noreply.github.com>
2025-02-26 22:12:09 -0700

    update

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -1,10 +1,10 @@
-const temp = 60;
-const wind = 10;
+const temp = 30;
+const wind = 30;
 
-function findwinchill(temperature, windSpeed) {
+function findwinchill(temp, wind) {
 
   if (temp <= 50 && wind > 3) {
-    const windtemp = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(temp, 0.16)) + Math.pow((0.4275 * temp * wind), 0.16);
+    const windtemp = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp) * Math.pow(wind, 0.16);
     return windtemp;
   }
 
@@ -14,3 +14,5 @@ function findwinchill(temperature, windSpeed) {
   }
 }
 
+const winchill = findwinchill(temp, wind);
+document.getElementById("findwinchill").innerHTML = winchill;
