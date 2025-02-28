6022091874139b1051103db57382b515ffa47685
brendonbainbridge <156622397+brendonbainbridge@users.noreply.github.com>
2025-02-27 21:13:05 -0700

    updates

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+	// Function to handle form submission
+	function handleSubmit(event) {
+		// Prevent default form submission
+		event.preventDefault();
+
+		// Access form elements
+		let formt = event.target;
+		let formData = new FormData(formt);
+
+		// Display form element values
+		for (let pair of formData.entries()) {
+			console.log(pair[0] + ": " + pair[1]);
+		}
+	}
+
+	const form = document.querySelector("form");
+	form.addEventListener("submit", handleSubmit);
+});
+
+
+
+
 const rangevalue = document.getElementById("rangenumber");
-const range = document.getElementById("range");
+const range = document.getElementById("rate");
 
 // RANGE event listener
 range.addEventListener('change', displayRatingValue);
 range.addEventListener('input', displayRatingValue);
 
 function displayRatingValue() {
-    rangevalue.innerHTML = range.value;
+  rangevalue.innerHTML = range.value;
 }
