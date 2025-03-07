e139a7312696d4b8d76c876b7fb0276e3434803b
brendonbainbridge <156622397+brendonbainbridge@users.noreply.github.com>
2025-03-06 20:50:42 -0700

    updates

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,13 @@
 const baseURL = "https://brendonbainbridge.github.io/wdd230/";
+const linksURL = "https://brendonbainbridge.github.io/wdd230/data/links.json";
+
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
+  
+  getLinks();
+
+
