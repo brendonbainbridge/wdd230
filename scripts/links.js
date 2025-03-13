78ffe0857092da2c6ded6e73c717d42d069c1afd
brendonbainbridge <156622397+brendonbainbridge@users.noreply.github.com>
2025-03-13 01:12:24 -0600

    update

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,5 @@
 const baseURL = "https://brendonbainbridge.github.io/wdd230/";
-const linksURL = "";
+const linksURL = "https://brendonbainbridge.github.io/wdd230/data/links.json";
 
 async function getLinks() {
   const response = await fetch(linksURL);
@@ -30,56 +30,3 @@ function displayLinks(lessons) {
     container.appendChild(listItem);
   });
 }
-
-
-
-
-
-
-const url = 'https://brendonbainbridge.github.io/wdd230/data/links.json';
-const cards = document.querySelector('#cards');
-
-async function getCompanyData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.table(data.prophets);
-    displayCompanies(data.companies); // note that we reference the prophets array of the JSON data object, not just the object
-}
-
-getCompanyData();
-
-const displayCompanies = (companies) => {
-    companies.forEach((company) => {
-        // Create elements to add to the div.cards element
-        let card = document.createElement('section');
-        let name = document.createElement('h2');
-        let address = document.createElement('p');
-        let phone = document.createElement('p');
-        let website = document.createElement('a');
-        let membership_level = document.createElement('p');
-        let portrait = document.createElement('img');
-
-        // Build the h2 content out to show the prophet's full name
-        name.textContent = `${company.name}`;
-        address.textContent = `${company.address}`;
-        phone.textContent = `${company.phone}`;
-        website.textContent = `${company.website}`;
-        membership_level.textContent = `${company.membership_level}`;
-
-        // Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', company.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${company.name}`);
-        portrait.setAttribute('loading', 'lazy');
-
-        // Append the section(card) with the created elements
-        card.appendChild(name);
-        card.appendChild(address);
-        card.appendChild(phone);
-        card.appendChild(website);
-        card.appendChild(image);
-        card.appendChild(membership_level);
-
-        cards.appendChild(card);
-    }); // end of arrow function and forEach loop
-}
-
